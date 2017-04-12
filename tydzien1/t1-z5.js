18e7353cef3f08868b3979af8add95786a558ea5
karolcholewa <karol.ch@gmail.com>
2017-04-12 23:00:29 +0200

    Create t1-z5.js

---
+++ tydzien1/t1-z5.js
@@ -0,0 +1,17 @@
+/*Utwórz funkcję o nazwie getDate, która po wywołaniu zwróci aktualną, sformatowaną datę. Data powinna być w formacie dd.mm.rrrr, czyli np. 17.04.2017.Przykładowe użycie tej funkcji powinno wyglądać następująco:getDate(); // zwraca np. “17.04.2017”*/
+
+var getDate = function() {
+  var todd = new Date();
+  var dd = todd.getDate();
+  var mm = todd.getmm() + 1;
+  var yyyy = todd.getFullyyyy();
+  if(mm < 10){
+    mm = "0" + mm;
+    }
+  if(dd < 10) {
+    dd = "0" + dd;
+  }
+  return console.log(dd + "." + mm + "." + yyyy);
+};
+
+getDate();
