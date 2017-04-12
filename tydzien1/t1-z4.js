d8cd808fa457d74f78957e5510ab4ed2d6b98ec7
karolcholewa <karol.ch@gmail.com>
2017-04-12 22:58:36 +0200

    Create t1-z4.js

---
+++ tydzien1/t1-z4.js
@@ -0,0 +1,12 @@
+/*Stwórz funkcję o nazwie sum, która będzie przyjmowała jeden parametr, którym będzie tablica z liczbami. Funkcja ta powinna zsumować wszystkie liczby z przekazanej tablicy, a następnie zwrócić wynik takiej operacji.Przykładowe jej użycie powinno wyglądać tak:sum([1, 10, 5, 4]); // zwraca wartość 20*/
+
+var sum = function(array) {
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+    sum += array[i];
+    }
+  return sum; 
+}
+
+console.log(sum([22,10,5,4]));
+
