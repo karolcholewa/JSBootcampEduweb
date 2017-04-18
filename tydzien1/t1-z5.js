00bbc953905317582a155034e15066a66059009a
karolcholewa <karol.ch@gmail.com>
2017-04-19 00:20:44 +0200

    Comments added, formatted code

--- tydzien1/t1-z5.js
+++ tydzien1/t1-z5.js
@@ -1,17 +1,23 @@
 /*Utwórz funkcję o nazwie getDate, która po wywołaniu zwróci aktualną, sformatowaną datę. Data powinna być w formacie dd.mm.rrrr, czyli np. 17.04.2017.Przykładowe użycie tej funkcji powinno wyglądać następująco:getDate(); // zwraca np. “17.04.2017”*/
+(function() {
+  
+    function getDate() {
 
-function getDate() {
-  var todd = new Date();
-  var dd = todd.getDate();
-  var mm = todd.getmm() + 1;
-  var yyyy = todd.getFullyyyy();
-  if(mm < 10){
-    mm = "0" + mm;
-    }
-  if(dd < 10) {
-    dd = "0" + dd;
-  }
-  return (dd + "." + mm + "." + yyyy);
-};
+      var today = new Date(); //create a new object from a Date constructor so all the methods are available to use
+      var dd = today.getDate(); //The getDate() method returns the day of the month for the specified date according to local time.
+      var mm = today.getMonth() + 1; //The getMonth() method returns the month in the specified date according to local time, as a zero-based value (where zero indicates the first month of the year).
+      var yyyy = today.getFullYear();//The getFullYear() method returns the year of the specified date according to local time.
 
-console.log(getDate());
+      if(mm < 10){
+        mm = "0" + mm;
+        }
+      if(dd < 10) {
+        dd = "0" + dd;
+      }
+
+      return (dd + "." + mm + "." + yyyy);
+
+    };
+
+    console.log(getDate());
+})();
